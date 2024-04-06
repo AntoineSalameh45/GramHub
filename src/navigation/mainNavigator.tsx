cd6a6af84382ebe2edc861d40e57c61beab06d51
BiG A <antoine.salameh.45@gmail.com>
2024-04-06 22:56:28 +0300

    removed unused lines

--- src/navigation/mainNavigator.tsx
+++ src/navigation/mainNavigator.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
 import MyTabBar from '../components/organisms/CustomTabBar';
-// import LoginScreen from '../screens/LoginScreen';
 import SearchScreen from '../screens/SearchScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 
@@ -21,11 +20,6 @@ const MainNavigator = () => {
         component={SearchScreen}
         options={{headerShown: false}}
       />
-      {/* <MainStackNavigator.Screen
-        name="Login"
-        component={LoginScreen}
-        options={{headerShown: false}}
-      /> */}
       <MainStackNavigator.Screen
         name="Profile"
         component={ProfileScreen}
