0e80cb2e929425fe8b821ac31374e0f8d70b31c6
BiG A <antoine.salameh.45@gmail.com>
2024-04-06 21:32:43 +0300

    adding styles to a seperate file

--- src/screens/ProfileScreen/styles.tsx
+++ src/screens/ProfileScreen/styles.tsx
@@ -18,5 +18,25 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
     marginTop: 20,
   },
+  ffContainer: {flexDirection: 'row'},
+  ffCol: {flexDirection: 'column'},
+  ffNumbers: {fontSize: 22, textAlign: 'center'},
+  ffLabel: {textAlign: 'center', marginHorizontal: 10},
+  profileButtons: {
+    padding: 10,
+    paddingHorizontal: 45,
+    borderRadius: 11,
+    backgroundColor: '#444',
+  },
+  profileButtonsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 30,
+  },
+  buttonTitles: {color: '#F3F8FF'},
+  postsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
 });
 export default styles;
