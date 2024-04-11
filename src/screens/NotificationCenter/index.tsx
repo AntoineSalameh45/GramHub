aa44e58c0d5c141c02228193030804c20ed257bd
BiG A <antoine.salameh.45@gmail.com>
2024-04-11 18:38:23 +0300

    keeping the notif displayed

--- src/screens/NotificationCenter/index.tsx
+++ src/screens/NotificationCenter/index.tsx
@@ -1,12 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import {View, FlatList, Text, Image, Pressable} from 'react-native';
 import notifee, {EventType} from '@notifee/react-native';
-import axios, {AxiosResponse} from 'axios';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import styles from './styles';
 import ClearSvg from '../../assets/svg/ClearSvg.svg';
 import NewNotif from '../../assets/svg/BellPlusSvg.svg';
 
-interface iUserData {
+export interface iUserData {
   createdAt: number;
   name: string;
   avatar: string;
@@ -15,7 +16,7 @@ interface iUserData {
   id: string;
 }
 
-interface iNotification {
+export interface iNotification {
   id: string;
   message: string;
   user: iUserData;
@@ -27,9 +28,6 @@ const NotifCenter = ({navigation}: any) => {
 
   useEffect(() => {
     fetchUserData();
-    const interval = setInterval(() => {
-      onDisplayNotification();
-    }, 60000);
 
     const backgroundEventHandler = async ({type}: {type: EventType}) => {
       if (type === EventType.PRESS) {
@@ -42,14 +40,14 @@ const NotifCenter = ({navigation}: any) => {
 
     notifee.onBackgroundEvent(backgroundEventHandler);
 
-    return () => {
-      clearInterval(interval);
-    };
+    retrieveNotifications();
+
+    return () => {};
   }, []);
 
   async function fetchUserData() {
     try {
-      const response: AxiosResponse<iUserData[]> = await axios.get(
+      const response = await axios.get(
         'https://6617aab9ed6b8fa43483619c.mockapi.io/api/hub/posts',
       );
       const data: iUserData[] = response.data;
@@ -69,34 +67,64 @@ const NotifCenter = ({navigation}: any) => {
         vibrationPattern: [500, 1000],
       });
 
-      if (userData && userData.length > 0) {
-        const randomIndex = Math.floor(Math.random() * userData.length);
-        const user = userData[randomIndex];
-        const message = `${user.name} just posted a photo`;
-        await notifee.displayNotification({
-          title: 'New Notification',
-          body: message,
-          android: {
-            channelId,
-            vibrationPattern: [500, 1000],
-            pressAction: {
-              id: 'default',
-            },
-          },
-        });
+      const storedNotifications = await AsyncStorage.getItem('notifications');
+      const parsedNotifications: iNotification[] = storedNotifications
+        ? JSON.parse(storedNotifications)
+        : [];
 
-        setNotifications(prevNotifications => [
-          ...prevNotifications,
-          {id: Math.random().toString(), message, user},
-        ]);
-      }
+      const updatedNotifications = [
+        ...parsedNotifications,
+        {
+          id: Math.random().toString(),
+          message: 'New notification',
+          user: userData[Math.floor(Math.random() * userData.length)],
+        },
+      ];
+
+      await AsyncStorage.setItem(
+        'notifications',
+        JSON.stringify(updatedNotifications),
+      );
+
+      setNotifications(updatedNotifications);
+
+      const randomIndex = Math.floor(
+        Math.random() * updatedNotifications.length,
+      );
+      const user = updatedNotifications[randomIndex].user;
+      const message = `${user.name} just posted a photo`;
+      await notifee.displayNotification({
+        title: 'New Notification',
+        body: message,
+        android: {
+          channelId,
+          vibrationPattern: [500, 1000],
+          pressAction: {
+            id: 'default',
+          },
+        },
+      });
     } catch (error) {
       console.error('Error displaying notification:', error);
     }
   }
 
-  const handleClear = () => {
+  const handleClear = async () => {
+    await AsyncStorage.removeItem('notifications');
     setNotifications([]);
+  };
+
+  const retrieveNotifications = async () => {
+    try {
+      const storedNotifications = await AsyncStorage.getItem('notifications');
+      if (storedNotifications !== null) {
+        const parsedNotifications: iNotification[] =
+          JSON.parse(storedNotifications);
+        setNotifications(parsedNotifications);
+      }
+    } catch (error) {
+      console.error('Error retrieving notifications:', error);
+    }
   };
 
   const renderItem = ({item}: {item: iNotification}) => {
@@ -111,13 +139,11 @@ const NotifCenter = ({navigation}: any) => {
 
   return (
     <View style={styles.viewContainer}>
-      <Pressable
-        onPress={() => onDisplayNotification()}
-        style={styles.bellButton}>
+      <Pressable onPress={onDisplayNotification} style={styles.bellButton}>
         <NewNotif width={30} height={30} />
       </Pressable>
 
-      <Pressable onPress={() => handleClear()} style={styles.clearButton}>
+      <Pressable onPress={handleClear} style={styles.clearButton}>
         <ClearSvg width={30} height={30} />
       </Pressable>
       <FlatList
